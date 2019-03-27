7e76800d955ec4d0cee9b7be56360afed5218a71
Miles <miles.b.flo@gmail.com>
2019-03-27 00:05:17 -0700

    Adding faction lookup page with static data for now

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { Redirect, Route, Router, Switch } from "react-router";
 import "./css/index.css";
 
+import Faction from './views/Faction';
 import Home from "./views/Home";
 import Login from "./views/Login";
 import Register from "./views/Register";
@@ -15,6 +16,7 @@ class App extends React.Component {
           <Route exact={true} path="/" component={Home} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
+          <Route path="/faction" component={Faction} />
           <Redirect from='*' to='/' />
         </Switch>
       </Router>
---
+++ src/views/Faction.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import Container from "../components/Container"
+import Header from "../components/Header"
+
+interface IFactionState {
+  factionFound: boolean;
+  id?: number;
+  name?: string;
+  color?: string;
+}
+
+class Faction extends React.Component {
+  public state: IFactionState;
+
+  constructor(props: any) {
+    super(props);
+
+    // TODO: faction lookup here
+    this.state = {
+      color: "#333333",
+      factionFound: true,
+      id: 0,
+      name: "testy"
+    };
+  }
+
+  public render() {
+    return (
+      <div>
+        <Header active="faction" />
+        <Container>
+          <h1>Faction</h1>
+          {this.state.factionFound
+            ? this.renderFactionData()
+            : this.factionNotFound()
+          }
+        </Container>
+      </div>
+    );
+  }
+
+  public renderFactionData() {
+    return (
+      <div>
+        <h2>ID</h2>
+        <p>{this.state.id}</p>
+        <h2>Name</h2>
+        <p>{this.state.name}</p>
+        <h2>Color</h2>
+        <p>{this.state.color}</p>
+      </div>
+    )
+  }
+  public factionNotFound() {
+    return (
+      <div>
+        <h1>Faction not found</h1>
+      </div>
+    )
+  }
+}
+
+export default Faction;
