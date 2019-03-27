8b960ef727651240a22db9ac5b3c2faa42a64d68
Miles <miles.b.flo@gmail.com>
2019-03-26 18:12:26 -0700

    Templating like that was something I was trying to get away with. Better to do it this way

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
+import { createBrowserHistory } from "history";
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import { Redirect, Route, Router, Switch } from "react-router";
 import './css/index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+import Home from "./views/Home";
+import Login from "./views/Login";
+import Register from "./views/Register";
+
 ReactDOM.render(
-  <App />,
+  <Router history={createBrowserHistory()}>
+    <Switch>
+      <Route exact={true} path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register}/>
+      <Redirect from='*' to='/' />
+    </Switch>
+  </Router>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
