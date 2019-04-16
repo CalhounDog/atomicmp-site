4f0954d2e65de00d4df80ad2c917b0c7e1bdad1e
Miles <miles.b.flo@gmail.com>
2019-04-16 02:01:06 -0700

    Adding 1\-5 themes for map

---
+++ src/utils/locations.ts
@@ -0,0 +1,108 @@
+import {
+  blockade,
+  cityRuins,
+  clover,
+  farm,
+  junkCity,
+  militaryCheckpoint,
+  radioStation,
+  railroadCrossing,
+  redRocket,
+  smallHouse,
+  tentCity,
+  town,
+} from "../images/map-markers"
+
+export default [
+  {
+    icon: clover,
+    id: "Ways Bar",
+    x: 1570,
+    y: 1160,
+  },
+  {
+    icon: militaryCheckpoint,
+    id: "Military Checkpoint",
+    x: 1163,
+    y: 458,
+  },
+  {
+    icon: smallHouse,
+    id: "Diner",
+    x: 1540,
+    y: 655,
+  },
+  {
+    icon: farm,
+    id: "Tumbleweed Fields",
+    x: 224,
+    y: 1799,
+  },
+  {
+    icon: smallHouse,
+    id: "The Oasis Motel",
+    x: 1620,
+    y: 1145,
+  },
+  {
+    icon: tentCity,
+    id: "NCR Encampment",
+    x: 542,
+    y: 282,
+  },
+  {
+    icon: railroadCrossing,
+    id: "The Tracks",
+    x: 505,
+    y: 820,
+  },
+  {
+    icon: radioStation,
+    id: "Military Broadcast Station",
+    x: 1890,
+    y: 1450,
+  },
+  {
+    icon: redRocket,
+    id: "Red Rocket",
+    x: 1370,
+    y: 605,
+  },
+  {
+    icon: blockade,
+    id: "Raider Encampment",
+    x: 1051,
+    y: 487,
+  },
+  {
+    icon: cityRuins,
+    id: "Fireside Arena",
+    x: 820,
+    y: 1050,
+  },
+  {
+    icon: farm,
+    id: "Ramsey Ranch",
+    x: 313,
+    y: 505,
+  },
+  {
+    icon: junkCity,
+    id: "Spawn Town",
+    x: 1720,
+    y: 720,
+  },
+  {
+    icon: farm,
+    id: "Stables",
+    x: 416,
+    y: 1040,
+  },
+  {
+    icon: town,
+    id: "Tri-Rail",
+    x: 420,
+    y: 965,
+  },
+
+]
--- src/views/Map.tsx
+++ src/views/Map.tsx
@@ -1,20 +1,7 @@
 import * as React from "react";
 import IUser from 'src/models/IUser';
-import {
-  blockade,
-  cityRuins,
-  clover,
-  farm,
-  junkCity,
-  militaryCheckpoint,
-  radioStation,
-  railroadCrossing,
-  redRocket,
-  smallHouse,
-  tentCity,
-  town,
-} from "../images/map-markers"
 import mapBackground from "../images/map.png";
+import locations from "../utils/locations"
 
 interface IMapLocationProps {
   id: string;
@@ -86,8 +73,15 @@ class Map extends React.Component<IMapProps, IMapState> {
   constructor(props: IMapProps) {
     super(props);
     this.getMapStyle = this.getMapStyle.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
+  public componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress, false);
+  }
+  public componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress, false);
+  }
   public render() {
     const mapBackgroundColor = this.getMapStyle();
     return (
@@ -109,116 +103,48 @@ class Map extends React.Component<IMapProps, IMapState> {
             opacity: .80,
           } : {})}>
 
-          {this.loadLocationIcons()}
+          {this.renderLocationIcons()}
         </div>
 
       </div>
     );
   }
 
-  public loadLocationIcons() {
-    const data = [
-      {
-        icon: clover,
-        id: "Ways Bar",
-        x: 1570,
-        y: 1160,
-      },
-      {
-        icon: militaryCheckpoint,
-        id: "Military Checkpoint",
-        x: 1163,
-        y: 458,
-      },
-      {
-        icon: smallHouse,
-        id: "Diner",
-        x: 1540,
-        y: 655,
-      },
-      {
-        icon: farm,
-        id: "Tumbleweed Fields",
-        x: 224,
-        y: 1799,
-      },
-      {
-        icon: smallHouse,
-        id: "The Oasis Motel",
-        x: 1620,
-        y: 1145,
-      },
-      {
-        icon: tentCity,
-        id: "NCR Encampment",
-        x: 542,
-        y: 282,
-      },
-      {
-        icon: railroadCrossing,
-        id: "The Tracks",
-        x: 505,
-        y: 820,
-      },
-      {
-        icon: radioStation,
-        id: "Military Broadcast Station",
-        x: 1890,
-        y: 1450,
-      },
-      {
-        icon: redRocket,
-        id: "Red Rocket",
-        x: 1370,
-        y: 605,
-      },
-      {
-        icon: blockade,
-        id: "Raider Encampment",
-        x: 1051,
-        y: 487,
-      },
-      {
-        icon: cityRuins,
-        id: "Fireside Arena",
-        x: 820,
-        y: 1050,
-      },
-      {
-        icon: farm,
-        id: "Ramsey Ranch",
-        x: 313,
-        y: 505,
-      },
-      {
-        icon: junkCity,
-        id: "Spawn Town",
-        x: 1720,
-        y: 720,
-      },
-      {
-        icon: farm,
-        id: "Stables",
-        x: 416,
-        y: 1040,
-      },
-      {
-        icon: town,
-        id: "Tri-Rail",
-        x: 420,
-        y: 965,
-      },
-
-    ]
+  public renderLocationIcons() {
     return (
       <svg xmlns="http://www.w3.org/2000/svg"
         width="100%" height="100%"
         viewBox="0 0 2048 2048"
         id="map-svg"
         >
-        {data.map(locationData => <MapLocation id={locationData.id} icon={locationData.icon} key={locationData.id} x={locationData.x} y={locationData.y}/>)}
+        {locations.map(locationData => <MapLocation id={locationData.id} icon={locationData.icon} key={locationData.id} x={locationData.x} y={locationData.y}/>)}
       </svg>
     )
+  }
+
+  public handleKeyPress(event: KeyboardEvent) {
+    switch(event.keyCode) {
+      case(49): {
+        this.setState({mode: "realistic"});
+        break;
+      }
+      case(50): {
+        this.setState({mode: "green"});
+        break;
+      }
+      case(51): {
+        this.setState({mode: "amber"});
+        break;
+      }
+      case(52): {
+        this.setState({mode: "blue"});
+        break;
+      }
+      case(53): {
+        this.setState({mode: "white"});
+        break;
+      }
+    }
   }
 
   private getMapStyle(): (string | undefined) {
