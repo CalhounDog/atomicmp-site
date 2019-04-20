d9019e824e636632702a8955aea3154932da1cf7
Miles <miles.b.flo@gmail.com>
2019-04-19 18:22:59 -0700

    Nuking deprecated lines from previous implementation

--- src/components/MapLocation.tsx
+++ src/components/MapLocation.tsx
@@ -11,36 +11,29 @@ interface IMapLocationProps {
   y: number;
 }
 
+// tslint:disable-next-line: no-empty-interface
 interface IMapLocationState {
-  hover: boolean;
 }
 
 class MapLocation extends React.Component<IMapLocationProps, Partial<IMapLocationState>> {
-  public state = {
-    hover: false
-  }
+  public state = {}
 
   constructor(props: IMapLocationProps) {
     super(props);
-    this.handleOnMouseEnter = this.handleOnMouseEnter.bind(this)
-    this.handleOnMouseLeave = this.handleOnMouseLeave.bind(this)
-    this.handleDragStart = this.handleDragStart.bind(this)
   }
 
   public render() {
     const iconDimension = 40;
 
     const locationIcon: Icon = new L.Icon({
-      // className: 'leaflet-div-icon',
       iconAnchor: undefined,
-      iconSize: new L.Point(iconDimension, iconDimension),
-// tslint:disable-next-line: object-literal-sort-keys
       iconRetinaUrl: this.props.icon,
+      iconSize: new L.Point(iconDimension, iconDimension),
       iconUrl: this.props.icon,
       popupAnchor: undefined,
-      shadowUrl: undefined,
-      shadowSize: undefined,
       shadowAnchor: undefined,
+      shadowSize: undefined,
+      shadowUrl: undefined,
     });
 
     const location = mapImagePointToLatLng({ x: this.props.x, y: this.props.y})
@@ -51,34 +44,8 @@ class MapLocation extends React.Component<IMapLocationProps, Partial<IMapLocatio
         <Popup>
           <span>{this.props.id}</span>
         </Popup><title>{this.props.id}</title>
-        {/* <image id={this.props.id}
-          style={{
-            WebkitFilter: "none",
-            boxShadow: "1px 1px black",
-            opacity: 1,
-          }}
-          onDragStart={this.handleDragStart}
-          stroke="#1aff80"
-          strokeWidth="2"
-          width={iconDimension + "px"}
-          height={iconDimension + "px"}
-          x={this.props.x - iconDimension / 2}
-          y={this.props.y - iconDimension / 2}
-          onMouseEnter={this.handleOnMouseEnter}
-          onMouseLeave={this.handleOnMouseLeave}
-          xlinkHref={this.props.icon}
-        /> */}
       </Marker>
     )
-  }
-  public handleOnMouseEnter() {
-    this.setState({ hover: true })
-  }
-  public handleOnMouseLeave() {
-    this.setState({ hover: false })
-  }
-  public handleDragStart(event: any) {
-    event.preventDefault();
   }
 }
 
