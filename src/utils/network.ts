d4bb996bb669a7d7a76d6a57a0de48ac89c72244
Miles <14067660+milesflo@users.noreply.github.com>
2019-05-16 15:21:11 -0700

    Created faction data lookup view

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,11 +1,28 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 
 
 const REACT_APP_BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "";
 
-const backend = axios.create({
+const BASE_CONFIG = {
   baseURL: REACT_APP_BACKEND_URL,
   responseType: 'json',
-})
+  timeout: 5000,
+}
+
+const backend: AxiosInstance = axios.create(BASE_CONFIG)
+
+const authConstructor = function () {
+  const token = window.sessionStorage.getItem("authToken");
+  if (token) {
+    return axios.create(Object.assign(BASE_CONFIG, {
+      headers: { Authorization: `Bearer ${token}` },
+    }))
+  } else {
+    return axios.create({})
+  }
+}
+
+const auth = authConstructor()
 
 export default backend;
+export { auth, backend };
